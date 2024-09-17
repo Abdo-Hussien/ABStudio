483abce2f2f98db4e12c736e974998fae82d0af8
Abdo-Hussien <abdelrahman.hsharaf@gmail.com>
2024-09-17 18:27:50 +0300

    Making requests with axios.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,54 @@
 
+import axios from 'axios'
 import '../App.scss'
+import { useEffect, useState } from 'react';
 
 
 function About() {
+    const [stories, setStories] = useState({})
+    const [error, setError] = useState({})
+
+    const handleCall = async () => {
+        try {
+            const { data, status } = await axios.get(`https://gateway.marvel.com/v1/public/comics/948?apikey=ffebf262c2bca06808eed12570dc894e`)
+            console.log(data.data.results[0])
+            console.log(status)
+            if (status === 200) {
+                setStories(data.data.results[0])
+            }
+            else {
+                setError(error?.message)
+                console.log(error)
+            }
+        } catch (err) {
+            console.log(`Error fetching result: ${err}`)
+        }
+    }
+
+    // useEffect to run the fetchData function when the component mounts
+    useEffect(() => {
+        handleCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []); // Empty dependency array means it runs once after the component mounts
+
+
     return (
         <div className="App">
             <div className="container">
-                <span>About</span>
+                <div>
+                    <button>Load Marvel Characters</button>
+                    <span className="text-3xl text-fuchsia-50">
+                        {stories?.dates?.map((date, index) => {
+                            return (
+                                <>
+                                    <p key={date.index}>
+                                        {date.type} <span>{date.date}</span>
+                                    </p>
+                                </>
+                            )
+                        })}
+                    </span>
+                </div>
 
             </div>
             <div className="subcontainer">
